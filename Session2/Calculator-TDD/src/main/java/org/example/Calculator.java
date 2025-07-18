685730d3dbb7bc9caa7114243dabbac71fe5a6e9
cade + paolo <cade+paolo@gcash.com>
2025-07-18 13:21:49 +0800

    Session 2 - Exercise: Calculator Developed TDD-Style Submission

--- Session2/Calculator-TDD/src/main/java/org/example/Calculator.java
+++ Session2/Calculator-TDD/src/main/java/org/example/Calculator.java
@@ -1,99 +1,93 @@
-package org.example;
+    package org.example;
 
-public class Calculator {
-    //addition
-    public int sum(int a, int b){
-        return a+b;
-    }
-
-    public long sum(long a, long b){
-        return a+b;
-    }
-
-    public short sum(short a, short b){
-        return (short) (a+b);
-    }
-
-    public float sum(float a, float b){
-        return a+b;
-    }
-
-    public double sum(double a, double b){
-        return a+b;
-    }
-    //difference
-    public int difference(int a, int b){
-        return a-b;
-    }
-    public long difference(long a, long b){
-        return a-b;
-    }
-    public short difference(short a, short b){
-        return (short)(a-b);
-    }
-    public float difference(float a, float b){
-        return a-b;
-    }
-    public double difference(double a, double b){
-        return a-b;
-    }
-
-    //multiplication
-    public int prod(int a, int b){
-        return a*b;
-    }
-
-    public long prod(long a, long b){
-        return a*b;
-    }
-
-    public short prod(short a, short b){
-        return (short)(a*b);
-    }
-
-    public float prod(float a, float b){
-        return a*b;
-    }
-
-    public double prod(double a, double b){
-        return a*b;
-    }
-
-    //division
-    public int div(int a, int b){
-        if (a == 0 || b == 0){
-            return -1;
+    public class Calculator {
+        //addition
+        public int sum(int a, int b){
+            return a+b;
         }
-        return a/b;
-    }
-    public float div(float a, float b){
-        if (a == 0 || b == 0){
-            return -1;
+
+        public long sum(long a, long b){
+            return a+b;
         }
-        return a/b;
-    }
-    public long div(long a, long b){
-        if (a == 0 || b == 0){
-            return -1;
+
+        public short sum(short a, short b){
+            return (short) (a+b);
         }
-        return a/b;
-    }
-    public short div(short a, short b){
-        if (a == 0 || b == 0){
-            return -1;
+
+        public float sum(float a, float b){
+            return a+b;
         }
-        return (short)(a/b);
-    }
-    public double div(double a, double b){
-        if (a == 0 || b == 0){
-            return -1;
+
+        public double sum(double a, double b){
+            return a+b;
         }
-        return a/b;
+        //difference
+        public int difference(int a, int b){
+            return a-b;
+        }
+        public long difference(long a, long b){
+            return a-b;
+        }
+        public short difference(short a, short b){
+            return (short)(a-b);
+        }
+        public float difference(float a, float b){
+            return a-b;
+        }
+        public double difference(double a, double b){
+            return a-b;
+        }
+
+        //multiplication
+        public int prod(int a, int b){
+            return a*b;
+        }
+
+        public long prod(long a, long b){
+            return a*b;
+        }
+
+        public short prod(short a, short b){
+            return (short)(a*b);
+        }
+
+        public float prod(float a, float b){
+            return a*b;
+        }
+
+        public double prod(double a, double b){
+            return a*b;
+        }
+
+        //division
+        public int div(int a, int b){
+            if (b == 0){
+                throw new IllegalArgumentException("Divisor cannot be zero.");
+            }
+            return a/b;
+        }
+        public float div(float a, float b){
+            if (b == 0){
+                throw new IllegalArgumentException("Divisor cannot be zero.");
+            }
+            return a/b;
+        }
+        public long div(long a, long b){
+            if (b == 0){
+                throw new IllegalArgumentException("Divisor cannot be zero.");
+            }
+            return a/b;
+        }
+        public short div(short a, short b){
+            if (b == 0){
+                throw new IllegalArgumentException("Divisor cannot be zero.");
+            }
+            return (short)(a/b);
+        }
+        public double div(double a, double b){
+            if (b == 0){
+                throw new IllegalArgumentException("Divisor cannot be zero.");
+            }
+            return a/b;
+        }
     }
-//    public float mod(float a, float b){
-//        if (a == 0 || b == 0){
-//            return -1;
-//        }
-//        return a%b;
-//    }
-}
--- Session2/Calculator-TDD/src/test/java/org/example/CalculatorTest.java
+++ Session2/Calculator-TDD/src/test/java/org/example/CalculatorTest.java
@@ -1,161 +1,158 @@
-package org.example;
+    package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+    import org.junit.jupiter.api.BeforeEach;
+    import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+    import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
-    private Calculator calculator;
+    class CalculatorTest {
+        private Calculator calculator;
 
-    @BeforeEach
-    void setup(){
-        calculator = new Calculator();
+        @BeforeEach
+        void setup(){
+            calculator = new Calculator();
+        }
+
+
+        //addition
+        @Test
+        void shouldReturnCorrectSumWhenAddingTwoIntegerNumbers() {
+            int actual = calculator.sum(2,1);
+            assertEquals(3, actual);
+        }
+
+        @Test
+        void shouldReturnCorrectSumWhenAddingTwoLongNumbers() {
+            long actual = calculator.sum(2_000_000_000L,100_000_000L);
+            assertEquals(2_100_000_000, actual);
+        }
+
+
+        @Test
+        void shouldReturnCorrectSumWhenAddingTwoShortNumbers() {
+            short a = -15_000;
+            short b = 16_000;
+            short actual = calculator.sum(a,b);
+            assertEquals(1_000, actual);
+        }
+
+
+        @Test
+        void shouldReturnCorrectSumWhenAddingTwoFloatNumbers() {
+            float actual = calculator.sum(5.35F,1.35F);
+            assertEquals(6.70F, actual,0.001);
+        }
+
+        @Test
+        void shouldReturnCorrectSumWhenAddingTwoDoubleNumbers() {
+            double actual = calculator.sum(2.80D,1.10D);
+            assertEquals(3.9D, actual, 0.01);
+        }
+
+        //subtraction
+        @Test
+        void shouldReturnCorrectDifferenceWhenSubtractingTwoIntegerNumbers() {
+            int actual = calculator.difference(2,1);
+            assertEquals(1, actual);
+        }
+
+        @Test
+        void shouldReturnCorrectDifferenceWhenSubtractingTwoLongNumbers() {
+            long actual = calculator.difference(2_000_000_000L,1_000_000_000L);
+            assertEquals(1_000_000_000, actual);
+        }
+
+        @Test
+        void shouldReturnCorrectDifferenceWhenSubtractingTwoShortNumbers() {
+            short a = 20_000;
+            short b = 15_000;
+            short actual = calculator.difference(a,b);
+            assertEquals(5_000, actual);
+        }
+
+        @Test
+        void shouldReturnCorrectDifferenceWhenSubtractingTwoFloatNumbers() {
+            float actual = calculator.difference(5.70F,1.35F);
+            assertEquals(4.35F, actual,0.001);
+        }
+
+        @Test
+        void shouldReturnCorrectDifferenceWhenSubtractingTwoDoubleNumbers() {
+            double actual = calculator.difference(2.80D,1.10D);
+            assertEquals(1.7D, actual, 0.01);
+        }
+
+
+        //multiplication
+        @Test
+        void shouldReturnCorrectProductWhenMultiplyingTwoIntegerNumbers(){
+            int actual = calculator.prod(10,30);
+            assertEquals(300, actual);
+        }
+
+        @Test
+        void shouldReturnCorrectProductWhenMultiplyingTwoLongNumbers(){
+            long actual = calculator.prod(10_000_000L,30L);
+            assertEquals(300_000_000L, actual);
+        }
+
+        @Test
+        void shouldReturnCorrectProductWhenMultiplyingTwoShortNumbers(){
+            short a = 2;
+            short b = 5_000;
+            short actual = calculator.prod(a,b);
+            assertEquals(10_000, actual);
+        }
+
+
+        @Test
+        void shouldReturnCorrectProductWhenMultiplyingTwoFloatNumbers(){
+            float actual = calculator.prod(5F, 3F);
+            assertEquals(15F, actual);
+        }
+
+        @Test
+        void shouldReturnCorrectProductWhenMultiplyingTwoDoubleNumbers(){
+            double actual = calculator.prod(5D, 3D);
+            assertEquals(15D, actual);
+        }
+
+        //Division
+        @Test
+        void shouldReturnCorrectQuotientWhenDividingTwoIntegerNumbers(){
+            int actual = calculator.div(15, 3);
+            assertEquals(5, actual);
+        }
+        @Test
+        void shouldThrowExceptionWhenDividingByZero() {
+            // Use assertThrows to verify that an exception is thrown
+            IllegalArgumentException thrownException = assertThrows(IllegalArgumentException.class, () -> {
+                calculator.div(15, 0);
+            });
+            assertEquals("Divisor cannot be zero.", thrownException.getMessage());
+        }
+
+        @Test
+        void shouldReturnCorrectQuotientWhenDividingTwoFloatNumbers(){
+            float actual = calculator.div(15.0f, 3.0f);
+            assertEquals(5.0f, actual);
+        }
+
+
+        @Test
+        void shouldReturnCorrectQuotientWhenDividingTwoShortNumbers(){
+            short actual = calculator.div((short)15_000, (short)3_000);
+            assertEquals(5, actual);
+        }
+        @Test
+        void shouldReturnCorrectQuotientWhenDividingTwoLongNumbers(){
+            long actual = calculator.div(15L, 3L);
+            assertEquals(5L, actual);
+        }
+        @Test
+        void shouldReturnCorrectQuotientWhenDividingTwoDoubleNumbers(){
+            double actual = calculator.div(15D, 3D);
+            assertEquals(5D, actual);
+        }
+
     }
-
-
-    //addition
-    @Test
-    void shouldReturnCorrectSumWhenAddingTwoIntegerNumbers() {
-        int actual = calculator.sum(2,1);
-        assertEquals(3, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectSumWhenAddingTwoLongNumbers() {
-        long actual = calculator.sum(2_000_000_000L,100_000_000L);
-        assertEquals(2_100_000_000, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectSumWhenAddingTwoShortNumbers() {
-        short a = -15_000;
-        short b = 16_000;
-        short actual = calculator.sum(a,b);
-        assertEquals(1_000, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectSumWhenAddingTwoFloatNumbers() {
-        float actual = calculator.sum(5.35F,1.35F);
-        assertEquals(6.70F, actual,0.001);
-    }
-
-    @Test
-    void shouldReturnCorrectSumWhenAddingTwoDoubleNumbers() {
-        double actual = calculator.sum(2.80D,1.10D);
-        assertEquals(3.9D, actual, 0.01);
-    }
-
-    //subtraction
-    @Test
-    void shouldReturnCorrectDifferenceWhenSubtractingTwoIntegerNumbers() {
-        int actual = calculator.difference(2,1);
-        assertEquals(1, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectDifferenceWhenSubtractingTwoLongNumbers() {
-        long actual = calculator.difference(2_000_000_000L,1_000_000_000L);
-        assertEquals(1_000_000_000, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectDifferenceWhenSubtractingTwoShortNumbers() {
-        short a = 20_000;
-        short b = 15_000;
-        short actual = calculator.difference(a,b);
-        assertEquals(5_000, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectDifferenceWhenSubtractingTwoFloatNumbers() {
-        float actual = calculator.difference(5.70F,1.35F);
-        assertEquals(4.35F, actual,0.001);
-    }
-
-    @Test
-    void shouldReturnCorrectDifferenceWhenSubtractingTwoDoubleNumbers() {
-        double actual = calculator.difference(2.80D,1.10D);
-        assertEquals(1.7D, actual, 0.01);
-    }
-
-
-    //multiplication
-    @Test
-    void shouldReturnCorrectProductWhenMultiplyingTwoIntegerNumbers(){
-        int actual = calculator.prod(10,30);
-        assertEquals(300, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectProductWhenMultiplyingTwoLongNumbers(){
-        long actual = calculator.prod(10_000_000L,30L);
-        assertEquals(300_000_000L, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectProductWhenMultiplyingTwoShortNumbers(){
-        short a = 2;
-        short b = 5_000;
-        short actual = calculator.prod(a,b);
-        assertEquals(10_000, actual);
-    }
-
-
-    @Test
-    void shouldReturnCorrectProductWhenMultiplyingTwoFloatNumbers(){
-        float actual = calculator.prod(5F, 3F);
-        assertEquals(15F, actual);
-    }
-
-    @Test
-    void shouldReturnCorrectProductWhenMultiplyingTwoDoubleNumbers(){
-        double actual = calculator.prod(5D, 3D);
-        assertEquals(15D, actual);
-    }
-
-    //Division
-    @Test
-    void shouldReturnCorrectQuotientWhenDividingTwoIntegerNumbers(){
-        int actual = calculator.div(15, 3);
-        assertEquals(5, actual);
-    }
-    @Test
-    void shouldReturnCorrectQuotientWhenDividingTwoLongNumbers(){
-        long actual = calculator.div(15.0f, 3.0f);
-        assertEquals(5, actual);
-    }
-    @Test
-    void shouldReturnCorrectQuotientWhenDividingTwoShortNumbers(){
-        short actual = calculator.div(15_000, 3_000);
-        assertEquals(5, actual);
-    }
-    @Test
-    void shouldReturnCorrectQuotientWhenDividingTwoFloatNumbers(){
-        float actual = calculator.div(15, 3);
-        assertEquals(5, actual);
-    }
-    @Test
-    void shouldReturnCorrectQuotientWhenDividingTwoDoubleNumbers(){
-        double actual = calculator.div(15D, 3D);
-        assertEquals(5, actual);
-    }
-
-//    @Test
-//    void shouldReturnCorrectRemainderWhenDividingTwoIntegerNumbers(){
-//        float actual = calculator.mod(12, 5);
-//        assertEquals(2, actual);
-//    }
-
-//    @Test
-//    void shouldNotReturnValueWhenDividingWithZero(){
-//        float actual = calculator.div(0, 0);
-//        assertEquals(-1, actual);
-//    }
-//    @Test
-//    void shouldNotReturnValueWhenGettingModuloWithZero(){
-//        float actual = calculator.mod(0, 0);
-//        assertEquals(-1, actual);
-//    }
-}
